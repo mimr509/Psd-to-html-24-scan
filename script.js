a9fd434c18c0ff6e438fcf75654cd193714989e0
mimr509 <mimr509@gmail.com>
2020-06-28 23:18:36 +0600

    add all data

---
+++ script.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+  $(".mobile-menu i").click(function () {
+    $(".menu-area").slideToggle();
+	});
+  new WOW().init();
+});
+
+//-- one page navigation.
+$('a[href^="#"]').click(function(event) {
+	var id = $(this).attr("href");
+	var target = $(id).offset().top;
+	$('html, body').animate({scrollTop:target}, 1000);
+	event.preventDefault();
+});
+
+function getTargetTop(elem){
+var id = elem.attr("href");
+var offset = 60;
+return $(id).offset().top - offset;
+}
+
+
+$(window).scroll(function(e){
+	isSelected($(window).scrollTop())
+});
+
+var sections = $('a[href^="#"]');
+
+function isSelected(scrolledTo){
+ 
+var threshold = 100;
+var i;
+
+for (i = 0; i < sections.length; i++) {
+	var section = $(sections[i]);
+	var target = getTargetTop(section);
+	 
+	if (scrolledTo > target - threshold && scrolledTo < target + threshold) {
+		sections.removeClass("active");
+		section.addClass("active");
+	}
+
+};
+}
+
